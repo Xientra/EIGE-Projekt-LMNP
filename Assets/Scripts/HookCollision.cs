84c5c3bff8c247fca8e5ef1d90e464ac63520188
Xirest <59026180+Xirest@users.noreply.github.com>
2020-01-16 19:30:25 +0100

    so einfach nochmal tryen

---
+++ Assets/Scripts/HookCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookCollision : MonoBehaviour
+{
+    private GrapplingHook grapplingHook;
+    private void Start()
+    {
+        grapplingHook = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<GrapplingHook>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        grapplingHook.collidedObject = other.gameObject;
+        grapplingHook.collisionDetected = true;
+    }
+}
