50382f062b0a58a24b490bba47f4eca751f238a8
Xirest <59026180+Xirest@users.noreply.github.com>
2020-01-27 22:41:51 +0100

    EndScreen und Lvl 3 Erweiterung

---
+++ Assets/Scripts/PressF.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressF : MonoBehaviour
+{
+    private bool canBeEnded = false;
+    private bool swappedScreen = false;
+    [SerializeField]
+    private GameObject endScreenTextMeshPro;
+    [SerializeField]
+    KeyCode exitKey = KeyCode.Escape;
+
+    void Update()
+    {
+        if(gameObject.GetComponent<Key>().pressState > 0)
+        {
+            swapToEndScreen();
+        }
+        if(Input.GetKeyDown(exitKey) && canBeEnded)
+        {
+            Application.Quit();
+        } 
+    }
+
+    private void swapToEndScreen()
+    {
+        if(!swappedScreen)
+        {
+            endScreen.SetActive(true);
+            swappedScreen = true;
+            canBeEnded = true;
+        }
+    }
+
+    
+}
